1653e868389f4a8c6ac609154150f6f2286ff53b
Marcio Sardou <marciosut1996@gmail.com>
2020-09-14 17:39:33 -0300

    adicionando links de redes sociais no header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../assets/pokedex-logo.png'
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
 import './styles.scss'
 
@@ -8,7 +9,20 @@ function Header() {
     <React.Fragment>
       <header>
           <img src={logo} className="img" alt="Pokedex Logo"/>
+          <aside>
+            <a href="https://www.instagram.com/marcio_sardou/" target="_blank">
+              <FaInstagram size="25"color="#252f83"/>
+            </a>
 
+            <a href="https://www.linkedin.com/in/marciosardou" target="_blank">
+            <FaLinkedin size="25"color="#252f83"/>
+            </a>
+
+            <a href="https://github.com/MarcioSardou" target="_blank">
+            <FaGithub size="25" color="#252f83"/>
+            </a>
+
+          </aside>
       </header>
     </React.Fragment>
   );
