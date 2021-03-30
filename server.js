015d70188da912150b6385c797d038369907fd71
marcio sardou <marciosut1996@gmail.com>
2021-03-30 00:27:02 -0300

    adicionando botão para montar time e contador de membros do time no header da aplicacao

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(bodyParser.json());
 
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
