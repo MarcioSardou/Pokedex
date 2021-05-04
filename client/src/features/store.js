c1ece7e0a5f4f199bfc750810f7136d3f7f5495b
unknown <marciosut1996@gmail.com>
2021-05-03 21:31:02 -0300

    teste

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import createSagaMiddleware  from 'redux-saga'
 import counterReducer from "./counter/counterSlice"
 import pokemonSquadReducer from "./pokemonSquad/pokemonSquadSlice"
 
-
-export default configureStore({
+const store = configureStore({
     reducer: {
         counter: counterReducer,
         pokemonSquad: pokemonSquadReducer
     }
 })
+
+  export default store;
+
+
+//   let sagaMiddleware = createSagaMiddleware();
+// const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
+
+// const store = configureStore({
+//     reducer: {
+//         counter: counterReducer,
+//         pokemonSquad: pokemonSquadReducer
+//     },
+//     middleware
+// })
+
+//   sagaMiddleware.run(saga);
